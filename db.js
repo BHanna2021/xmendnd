73dbac5ed740ae72f8c67e77d06a454cd0168b06
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-07-22 23:46:53 -0400

    still trying

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    // ssl: process.env.ENVIRONMENT === 'production'
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        }
-    }
+    ssl: process.env.ENVIRONMENT === 'production'
+    // dialectOptions: {
+    //     ssl: {
+    //         require: true,
+    //         rejectUnauthorized: false,
+    //     }
+    // }
 });
 
 const Express = require("express");
